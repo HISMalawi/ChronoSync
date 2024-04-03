fa79d0091665e75cad06561f0e0a5ddf62566b06
Roy Chanunkha <roychanunkha@gmail.com>
2024-04-03 20:23:23 +0200

    chore: adding a chrono service

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 import os
+import time
+
+def main():
+    counter = 0
+    while True:
+        # Your code here...
+        time.sleep(60)  # sleep for 60 seconds # should updated to use configured time
+        
+if __name__ == "__main__":
+    main()
