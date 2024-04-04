41e27b3a0d949b097bd3e509c71373ef5b2b0192
Pistone Junior Sanjama <pistonsanjama45@gmail.com>
2024-04-04 21:15:03 +0200

    chore: mapping the hasmap to the values

--- services/map_table.py
+++ services/map_table.py
@@ -8,27 +8,35 @@ class MapTable:
         self.queries = queries
         result = fetch_cursor(env)
         self.cursor = result[0]
-        self.column_hashmap = {}
+        self.post_data = []
         self.connection = result[1]
         self.map_query()
 
     def map_query(self):
         for query in self.queries:
-            table_name = self.extract_table_name(query)
-            query = f"SHOW COLUMNS FROM {table_name}"
-            self.cursor.execute(query)
+            table_name = query['table']
+            print(query['table'])
+            table_desc = f"SHOW COLUMNS FROM {table_name}"
+            self.cursor.execute(table_desc)
             columns_info = self.cursor.fetchall()
 
             # Create a hashmap of column names
+            column_hashmap = {}
             for index, column_info in enumerate(columns_info, start=1):
                 modified_index = f"@{index}"
-                self.column_hashmap[modified_index] = column_info['Field']  # Column name
-            print(self.column_hashmap)
+                column_hashmap[modified_index] = column_info['Field']  # Column name
 
-    def extract_table_name(self, query):
-        # Match INSERT INTO table_name or DELETE FROM table_name or UPDATE table_name
-        match = re.search(r'(?:INSERT INTO|DELETE FROM|UPDATE)\s+(\w+)', query)
-        if match:
-            return match.group(1)
-        return None
+            updated_data = {}  # Create a separate dictionary for updated data
+            for key, value in query["data"].items():
+                if key in column_hashmap:
+                    updated_data[column_hashmap[key]] = value
+                else:
+                    updated_data[key] = value  # If key not found in column_hashmap, keep it as it is | handle it here
+
+            query["data"] = updated_data  # Update the query's data dictionary
+
+            self.post_data.append(updated_data)
+        print(self.post_data)
+        return self.post_data
+
 
