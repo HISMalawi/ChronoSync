9f9a6605d9a2623fbc94cdcae2eb13b9fd377644
Roy Chanunkha <roychanunkha@gmail.com>
2024-04-04 22:06:09 +0200

    adding table manager and log converter

---
+++ services/log_converter.py
@@ -0,0 +1,49 @@
+def map_keys_to_columns(data, table_columns_map):
+    table_name = data.get('table')
+    data = data.get('data')
+    columns_map = table_columns_map.get(table_name, {})
+    return {columns_map.get(key, key): value for key, value in data.items()}
+
+# def main():
+#     data = {
+#   "operation": "DELETE",
+#   "database": "zomba",
+#   "table": "concept_name",
+#   "data": {
+#    "@1": "183",
+#    "@2": "manzy",
+#    "@3": "",
+#    "@4": "1",
+#    "@5": "2024-04-04 12:57:23",
+#    "@6": "106360",
+#    "@7": "0",
+#    "@8": "NULL",
+#    "@9": "NULL",
+#    "@10": "NULL",
+#    "@11": "e13d76ef-f282-11ee-a254-0242ac110005",
+#    "@12": "NULL",
+#    "@13": "0"
+#   }
+#  }
+#     table_columns_map = {
+#         "concept_name": {
+#             "@1": "id",
+#             "@2": "name",
+#             "@3": "age",
+#             "@4": "is_active",
+#             "@5": "created_at",
+#             "@6": "created_by",
+#             "@7": "is_deleted",
+#             "@8": "updated_by",
+#             "@10": "voided_by",
+#             "@11": "uuid",
+#             "@12": "description",
+#             "@13": "is_voided"
+#         }
+#     }
+#     print(map_keys_to_columns(data, table_columns_map))
+
+
+# if __name__ == "__main__":
+#     main()
+
---
+++ services/table_manager.py
@@ -0,0 +1,22 @@
+from utils.connector import fetch_cursor
+
+class TableManager:
+    def __init__(self, env):
+        self.env = env
+        result = fetch_cursor(env)
+        self.cursor = result[0]
+        self.connection = result[1]
+
+    def get_table_columns_map(self):
+        table_names = self.env['TRANS_TABLE'].split(',')
+        table_columns_map = {}
+        for table_name in table_names:
+            self.cursor.execute(f"DESCRIBE {table_name}")
+            columns = self.cursor.fetchall()
+            column_dict = {}
+            # now we want something like this {'@1': 'id', '@2': 'name', '@3': 'age'}
+            for index, column in enumerate(columns, start=1):
+                column_dict[f"@{index + 1}"] = column[index]
+            table_columns_map[table_name] = column_dict
+        
+        return table_columns_map
